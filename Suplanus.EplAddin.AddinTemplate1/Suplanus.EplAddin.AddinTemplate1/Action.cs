7e262bf53079e574e7f72ceda4bf5bd4c42c4bb1
Johann Weiher <info@suplanus.de>
2016-05-09 10:59:31 +0200

    Project added

---
+++ Suplanus.EplAddin.AddinTemplate1/Suplanus.EplAddin.AddinTemplate1/Action.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Eplan.EplApi.ApplicationFramework;
+
+namespace Suplanus.EplAddin.AddinTemplate1
+{
+   class Action : IEplAction
+   {
+      public void GetActionProperties(ref ActionProperties actionProperties) { }
+
+      public bool OnRegister(ref string name, ref int ordinal)
+      {
+         // ReSharper disable once PossibleNullReferenceException
+         name = MethodBase.GetCurrentMethod().DeclaringType.Name; // Get name from class
+         ordinal = 20;
+         return true;
+      }
+
+      public bool Execute(ActionCallingContext oActionCallingContext)
+      {
+
+         return true;
+      }
+   }
+}
---
+++ Suplanus.EplAddin.AddinTemplate1/Suplanus.EplAddin.AddinTemplate1/Addin.cs
@@ -0,0 +1,34 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+
+namespace Suplanus.EplAddin.AddinTemplate1
+{
+   public class AddInModule : IEplAddIn
+   {
+      public bool OnRegister(ref bool isLoadingOnStart)
+      {
+         isLoadingOnStart = true;
+         return true;
+      }
+
+      public bool OnUnregister()
+      {
+         return true;
+      }
+
+      public bool OnInit()
+      {
+         return true;
+      }
+
+      public bool OnInitGui()
+      {
+         return true;
+      }
+
+      public bool OnExit()
+      {
+         return true;
+      }
+
+   }
+}
